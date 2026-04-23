d550b6ceda3e46e2d31e68a4fff56b4219a348d7
Ali <alialashwall14@gmail.com>
2026-04-23 08:30:29 +0200

    collect the request parameters on a data class

---
+++ app/src/main/java/com/example/cineverse/domain/model/LoginRequest.kt
@@ -0,0 +1,12 @@
+package com.example.cineverse.domain.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LoginRequest(
+    val username: String,
+    val password: String,
+    @SerialName("request_token")
+    val requestToken: String
+)
