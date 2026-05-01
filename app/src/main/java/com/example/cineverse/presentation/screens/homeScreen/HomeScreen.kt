99178f855ff736b5f238eb0e98ecbef38c6c9dd9
Ali <alialashwall14@gmail.com>
2026-05-01 16:43:31 +0300

    passing the fetched movies list to the presentation layer

--- app/src/main/java/com/example/cineverse/presentation/screens/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/cineverse/presentation/screens/homeScreen/HomeScreen.kt
@@ -1,23 +1,23 @@
 package com.example.cineverse.presentation.screens.homeScreen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.example.cineverse.R
+import com.example.cineverse.domain.model.Movie
 import com.example.cineverse.domain.model.mockMoviesList
-import com.example.cineverse.navigation.EmptyRoute
+import com.example.cineverse.presentation.components.CineVerseErrorScreen
+import com.example.cineverse.presentation.components.CineVerseLoading
 import com.example.cineverse.presentation.designSystem.theme.CineVerseTheme
 import com.example.cineverse.presentation.designSystem.theme.Theme
 import com.example.cineverse.presentation.screens.homeScreen.components.HomeHeader
@@ -30,9 +30,47 @@ fun HomeScreen(
     navController: NavController,
     homeViewModel: HomeViewModel
 ) {
-    LaunchedEffect(Unit) {
-        homeViewModel.onStart()
+    val homeUiState by homeViewModel.homeUiState.collectAsStateWithLifecycle()
+    val homeEvent by homeViewModel.homeEvent.collectAsStateWithLifecycle()
+
+
+    when (homeEvent) {
+        HomeEvent.Loading -> { CineVerseLoading() }
+
+        HomeEvent.Success -> {
+            HomeScreenContainer(
+                modifier = modifier,
+                onClickCarouselMovie = {},
+                userName = homeUiState.userName,
+                upComingMovies = homeUiState.upcomingMovies,
+                topRatedMovies = homeUiState.topRatedMovies,
+                nowPlayingMovies = homeUiState.nowPlayingMovies,
+                popularMovies = homeUiState.popularMovies
+            )
+        }
+
+        HomeEvent.Error -> {
+            CineVerseErrorScreen(
+                onTryAgain = {}
+            )
+        }
+
     }
+
+
+}
+
+
+@Composable
+fun HomeScreenContainer(
+    modifier: Modifier = Modifier,
+    onClickCarouselMovie: () -> Unit,
+    userName: String,
+    upComingMovies: List<Movie>,
+    topRatedMovies: List<Movie>,
+    nowPlayingMovies: List<Movie>,
+    popularMovies: List<Movie>
+) {
     LazyColumn(
         modifier = modifier
             .fillMaxSize()
@@ -41,65 +79,73 @@ fun HomeScreen(
         contentPadding = PaddingValues(bottom = 24.dp)
     ) {
         item {
-            HomeHeader(userName = "Ali Gamal", modifier = Modifier.clickable {
-                navController.navigate(EmptyRoute)
-            })
+            HomeHeader(userName = userName)
         }
         item {
             HomeHeaderCarousel(
-                moviesList = mockMoviesList,
-                onClickMovie = {/*TO-DO*/ }
+                moviesList = upComingMovies,
+                onClickMovie = { onClickCarouselMovie() }
             )
         }
         item {
+            // Now Playing Section
             SuggestedSection(
                 title = stringResource(id = R.string.recently_released),
-                moviesList = mockMoviesList,
+                moviesList = nowPlayingMovies,
                 onShowMoreClicked = {}
             )
         }
 
         item {
+            // Up coming Section
             SuggestedSection(
                 title = stringResource(R.string.upcoming_movies),
-                moviesList = mockMoviesList,
+                moviesList = upComingMovies,
                 onShowMoreClicked = {}
             )
         }
 
         item {
+            // Popular Section
             SuggestedSection(
                 title = stringResource(R.string.matches_your_vibe),
-                moviesList = mockMoviesList,
+                moviesList = popularMovies,
                 onShowMoreClicked = {}
             )
         }
 
         item {
+            // Top Rated Section
             SuggestedSection(
                 title = stringResource(R.string.top_rated_tv_shows),
-                moviesList = mockMoviesList,
+                moviesList = topRatedMovies,
                 onShowMoreClicked = {}
             )
         }
 
         item {
+            // Temporal until get the recently viewed movies
             SuggestedSection(
                 title = stringResource(R.string.you_recently_viewed),
-                moviesList = mockMoviesList,
+                moviesList = popularMovies,
                 onShowMoreClicked = {}
             )
         }
     }
 }
 
-
 @Preview
 @Composable
 private fun HomeScreenPreview() {
-    val homeViewModel: HomeViewModel = hiltViewModel()
-    val navController = rememberNavController()
+
     CineVerseTheme {
-        HomeScreen(navController = navController, homeViewModel = homeViewModel)
+        HomeScreenContainer(
+            userName = "Ali Gamal",
+            onClickCarouselMovie = {},
+            upComingMovies = mockMoviesList,
+            topRatedMovies = mockMoviesList,
+            nowPlayingMovies = mockMoviesList,
+            popularMovies = mockMoviesList
+        )
     }
 }
