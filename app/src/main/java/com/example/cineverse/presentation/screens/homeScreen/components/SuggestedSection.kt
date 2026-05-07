92236dd4915ee766de150c1e568e869323d71b0e
Ali <alialashwall14@gmail.com>
2026-05-07 13:03:25 +0300

    separate the show more row in a separate file to be used as a component

--- app/src/main/java/com/example/cineverse/presentation/screens/homeScreen/components/SuggestedSection.kt
+++ app/src/main/java/com/example/cineverse/presentation/screens/homeScreen/components/SuggestedSection.kt
@@ -1,30 +1,23 @@
 package com.example.cineverse.presentation.screens.homeScreen.components
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.example.cineverse.R
 import com.example.cineverse.domain.model.Movie
 import com.example.cineverse.presentation.components.MovieCard
-import com.example.cineverse.presentation.designSystem.theme.Theme
+import com.example.cineverse.presentation.components.ShowMoreRow
 import kotlinx.coroutines.launch
 
 
@@ -59,31 +52,13 @@ fun SuggestedSection(
         modifier = modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = title,
-                style = Theme.textStyle.titleSm,
-                color = Theme.colors.shadePrimary
-            )
 
-            if (shouldShowMoreButton) {
-                Text(
-                    text = stringResource(id = R.string.show_more),
-                    style = Theme.textStyle.bodyMdMedium,
-                    color = Theme.colors.brandPrimary,
-                    modifier = Modifier.clickable(enabled = !isExpanded) {
-                        handleShowMore()
-                    }
-                )
-            }
-        }
-
+        ShowMoreRow(
+            title = title,
+            shouldShowMoreButton = shouldShowMoreButton,
+            isExpanded = isExpanded,
+            handleShowMore = { handleShowMore() }
+        )
         LazyRow(
             state = listState,
             contentPadding = PaddingValues(horizontal = 12.dp),
