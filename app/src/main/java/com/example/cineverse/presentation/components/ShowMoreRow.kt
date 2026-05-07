cab4313bae15131648f5335d725ef4e95e7b4705
Ali <alialashwall14@gmail.com>
2026-05-07 13:03:03 +0300

    separate the show more row in a separate file to be used as a component

---
+++ app/src/main/java/com/example/cineverse/presentation/components/ShowMoreRow.kt
@@ -0,0 +1,49 @@
+package com.example.cineverse.presentation.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.cineverse.R
+import com.example.cineverse.presentation.designSystem.theme.Theme
+
+@Composable
+fun ShowMoreRow(
+    modifier: Modifier = Modifier,
+    title: String,
+    shouldShowMoreButton: Boolean,
+    isExpanded: Boolean,
+    handleShowMore: () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = title,
+            style = Theme.textStyle.titleSm,
+            color = Theme.colors.shadePrimary
+        )
+
+        if (shouldShowMoreButton) {
+            Text(
+                text = stringResource(id = R.string.show_more),
+                style = Theme.textStyle.bodyMdMedium,
+                color = Theme.colors.brandPrimary,
+                modifier = Modifier.clickable(enabled = !isExpanded) {
+                    handleShowMore()
+                }
+            )
+        }
+    }
+}
