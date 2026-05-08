39d9b9bba8c2997f0c16c7b1fc8d8e53c148bfb7
Ali <alialashwall14@gmail.com>
2026-05-08 16:14:12 +0300

    separate the show more row in a separate file to be used as a component

--- app/src/main/java/com/example/cineverse/presentation/components/ShowMoreRow.kt
+++ app/src/main/java/com/example/cineverse/presentation/components/ShowMoreRow.kt
@@ -7,9 +7,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -22,10 +19,8 @@ fun ShowMoreRow(
     modifier: Modifier = Modifier,
     title: String,
     shouldShowMoreButton: Boolean,
-    isExpanded: Boolean,
     handleShowMore: () -> Unit
 ) {
-    val isReviewsExpanded by remember { mutableStateOf(isExpanded) }
     Row(
         modifier = modifier
             .fillMaxWidth()
@@ -44,9 +39,7 @@ fun ShowMoreRow(
                 text = stringResource(id = R.string.show_more),
                 style = Theme.textStyle.bodyMdMedium,
                 color = Theme.colors.brandPrimary,
-                modifier = Modifier.clickable(enabled = !isReviewsExpanded) {
-                    handleShowMore()
-                }
+                modifier = Modifier.clickable { handleShowMore() }
             )
         }
     }
