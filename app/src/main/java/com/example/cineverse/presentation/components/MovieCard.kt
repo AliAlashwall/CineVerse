af4a004db431ac80effa829ee5e8c1d1ef504f68
Ali <alialashwall14@gmail.com>
2026-07-07 03:56:42 +0300

    pass the movie id while clicking it

--- app/src/main/java/com/example/cineverse/presentation/components/MovieCard.kt
+++ app/src/main/java/com/example/cineverse/presentation/components/MovieCard.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cineverse.domain.model.Movie
 import com.example.cineverse.presentation.designSystem.theme.Theme
@@ -25,7 +26,7 @@ import com.example.cineverse.presentation.designSystem.theme.Theme
 fun MovieCard(
     movie: Movie,
     modifier: Modifier = Modifier,
-    onMovieClicked: () -> Unit
+    onMovieClicked: (Int) -> Unit
 ) {
     Column(
         modifier = modifier.width(136.dp),
@@ -33,7 +34,7 @@ fun MovieCard(
     ) {
         Box(
             modifier = Modifier
-                .clickable { onMovieClicked() }
+                .clickable { onMovieClicked(movie.id) }
                 .fillMaxWidth()
                 .aspectRatio(0.74f)
                 .clip(RoundedCornerShape(12.dp))
@@ -59,4 +60,31 @@ fun MovieCard(
             overflow = TextOverflow.Ellipsis
         )
     }
+}
+
+@Preview
+@Composable
+private fun MovieCardPreview() {
+   MovieCard(
+       movie = Movie(
+           adult = false,
+           backdropPath = "/zfbjgQE1uSd9wiPTX4VzsLi0rGG.jpg",
+           genreIds = listOf(
+               18,
+               80
+           ),
+           id = 2787,
+           title = "The Shawshank Redemption",
+           originalLanguage = "en",
+           originalTitle = "The Shawshank Redemption",
+           overview = "Imprisoned in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.",
+           popularity = 48.5295,
+           posterPath = "/9cqNxx0GxF0bflZmeSMuL5tnGzr.jpg",
+           releaseDate = "1994-09-23",
+           softcore = false,
+           video = false,
+           voteAverage = 7.5,
+           voteCount = 30212
+       ),
+   ) { }
 }
