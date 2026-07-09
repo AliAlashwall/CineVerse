0480a2e8272bfa7604c91c2662143f9101758e57
Ali <alialashwall14@gmail.com>
2026-07-09 05:43:56 +0300

    add clickable fun for the search bar

--- app/src/main/java/com/example/cineverse/presentation/components/SearchBar.kt
+++ app/src/main/java/com/example/cineverse/presentation/components/SearchBar.kt
@@ -1,6 +1,7 @@
 package com.example.cineverse.presentation.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -20,14 +21,17 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cineverse.R
+import com.example.cineverse.presentation.designSystem.theme.CineVerseTheme
 import com.example.cineverse.presentation.designSystem.theme.Theme
 
 @Composable
 fun SearchBar(
     query: String,
-    onQueryChange: (String) -> Unit
+    onQueryChange: (String) -> Unit,
+    onClick: () -> Unit = {}
 ) {
     Box(
         modifier = Modifier
@@ -38,7 +42,11 @@ fun SearchBar(
             .padding(horizontal = 16.dp),
         contentAlignment = Alignment.CenterStart
     ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { onClick() }) {
             Icon(
                 painter = painterResource(id = R.drawable.outline_search),
                 contentDescription = stringResource(R.string.search_icon),
@@ -70,5 +78,17 @@ fun SearchBar(
                 modifier = Modifier.size(20.dp)
             )
         }
+    }
+}
+
+@Preview
+@Composable
+private fun SearchBarPreview() {
+    CineVerseTheme {
+        SearchBar(
+            query = "",
+            onQueryChange = {},
+            onClick = {}
+        )
     }
 }
