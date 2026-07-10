2cb6318ea9440b68ce038210460fa52b53b11b62
Ali <alialashwall14@gmail.com>
2026-07-10 03:08:38 +0300

    remove unwanted movies list

--- app/src/main/java/com/example/cineverse/presentation/screens/homeScreen/HomeUiState.kt
+++ app/src/main/java/com/example/cineverse/presentation/screens/homeScreen/HomeUiState.kt
@@ -1,14 +1,8 @@
 package com.example.cineverse.presentation.screens.homeScreen
 
 
-import com.example.cineverse.domain.model.Movie
-
 data class HomeUiState(
     val userName: String = "Ali Gamal",
     val isLoading: Boolean = false,
     val guestId: String? = null,
-    val upcomingMovies: List<Movie> = emptyList(),
-    val topRatedMovies: List<Movie> = emptyList(),
-    val nowPlayingMovies: List<Movie> = emptyList(),
-    val popularMovies: List<Movie> = emptyList(),
 )
